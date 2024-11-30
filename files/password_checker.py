eea2889a2d274aba274a06763295354560cb1568
texonace <texonace123@gmail.com>
2024-11-30 15:11:50 +0530

    refactored the code to pep-8

--- files/password_checker.py
+++ files/password_checker.py
@@ -7,22 +7,20 @@ class Passwords:
         res: requests = requests.get(url)
         if res.status_code != 200:
             raise RuntimeError(f'Error fetching from api: {res.status_code}')
-            exit(0)
         return res
 
     @staticmethod
     def hash_password_for_api(password: str) -> tuple:
-        tail: str = ''
-        sha1_passowrd: str = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-        sha1_passowrd, tail = sha1_passowrd[:5], sha1_passowrd[5:]
-        return sha1_passowrd, tail
+        sha1_password: str = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+        sha1_password, tail = sha1_password[:5], sha1_password[5:]
+        return sha1_password, tail
     
 
     @staticmethod
     def check_no_of_leaks(response: requests, tail: str) -> int:
         hashed_password = (line.split(':') for line in response.text.splitlines())
-        for hash, leak_count in hashed_password:
-            if hash == tail:
+        for hash_key, leak_count in hashed_password:
+            if hash_key == tail:
                 return leak_count
         return 0
             
